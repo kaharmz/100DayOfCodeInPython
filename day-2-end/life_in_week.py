a2b3cfd1b5166033f6ba7d1af64d16a78b99638a
kahar <abdkaharmuzakir@gmail.com>
2023-04-26 16:07:46 +0700

    Change variable name

--- day-2-end/life_in_week.py
+++ day-2-end/life_in_week.py
@@ -2,10 +2,10 @@
 age = input("What is your current age? ")
 
 #Write your code below this line
-turning = 90 - int(age)
-days = turning * int(365)
-weeks = turning * int(52)
-months = turning * int(12)
+until = 90 - int(age)
+days = until * int(365)
+weeks = until * int(52)
+months = until * int(12)
 
 #output
 print(f"You have {days} days, {weeks} weeks, and {months} months left.")
