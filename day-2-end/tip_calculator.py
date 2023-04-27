bf63a2dff7dc984f2809c054bdce7b1ffa24b336
kahar <abdkaharmuzakir@gmail.com>
2023-04-27 10:21:12 +0700

    Add new variable

--- day-2-end/tip_calculator.py
+++ day-2-end/tip_calculator.py
@@ -6,8 +6,11 @@
 
 #Write your code below this line
 print("Welcome to the tip calculator")
-bill = input("What was the total bill? $")
-split = input("What percentage tip would you like to give? 10, 12, 15? ")
-result = (float(bill) / int(split) * 1.12)
-rounded = round(result, 2)
-print(f"Each person should pay: ${rounded:.2f}")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, 15? "))
+people = int(input("How many people to split the bill?"))
+percent = tip / 100
+tip_amount = bill * percent
+total = bill + tip_amount / people
+rounded = round(total, 2)
+print(f"Each person should pay: ${rounded}")
