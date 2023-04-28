8a90eb8a2436dd8927f846bc7a0b67540ad922af
kahar <abdkaharmuzakir@gmail.com>
2023-04-28 16:23:24 +0700

    Add file pizza_order_practice

--- day-3-end/pizza_order_practice.py
+++ day-3-end/pizza_order_practice.py
@@ -5,4 +5,25 @@ add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 # Don't change the code above
 
+"""
+CLUE
+    Small Pizza: $15
+    Medium Pizza: $20
+    Large Pizza: $25
+    Pepperoni for Small Pizza: +$2
+    Pepperoni for Medium or Large Pizza: +$3
+    Extra cheese for any size pizza: + $1
+"""
+
 #Write your code below this line
+small_pizza = 15
+medium_pizza = 20
+large_pizza = 25
+
+if size == "S" and add_pepperoni == "Y":
+    total = small_pizza + 2
+    print(total)
+elif size == "M" or "L" and add_pepperoni == "Y":
+    total = medium_pizza + 3
+    print(total)
+
