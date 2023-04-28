c210830bad2a3ba57bf0a9c331698c2a4dd0fe17
kahar <abdkaharmuzakir@gmail.com>
2023-04-28 14:28:29 +0700

    Add file leap_year in day 3

--- day-3-end/leap_year.py
+++ day-3-end/leap_year.py
@@ -3,3 +3,6 @@ year = int(input("Which year do you want to check? "))
 # Don't change the code above
 
 #Write your code below this line
+#on every year that is evenly divisible by 4
+#'except' every year that is evenly divisible by 100
+#'unless' the year is also evenly divisible by 400
