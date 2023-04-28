18f617742a160d1150e23be693b7198d80439c42
kahar <abdkaharmuzakir@gmail.com>
2023-04-28 16:19:46 +0700

    Add file pizza_order_practice

---
+++ day-3-end/pizza_order_practice.py
@@ -0,0 +1,8 @@
+# Don't change the code below
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# Don't change the code above
+
+#Write your code below this line
