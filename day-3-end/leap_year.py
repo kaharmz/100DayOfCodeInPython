6fb6369e2eaaaa0c51b5b0e28ece381768a0c020
kahar <abdkaharmuzakir@gmail.com>
2023-04-28 14:51:18 +0700

    Use nested if for search leap year

--- day-3-end/leap_year.py
+++ day-3-end/leap_year.py
@@ -4,11 +4,11 @@ year = int(input("Which year do you want to check? "))
 
 #Write your code below this line
 
-#on every year that is evenly divisible by 4
+#every year that is evenly divisible by 4
 if year % 4 == 0:
-    # 'except' every year that is evenly divisible by 100
+    # every year that is evenly divisible by 100
     if year % 100 == 0:
-        # 'unless' the year is also evenly divisible by 400
+        # the year is also evenly divisible by 400
         if year % 400 == 0:
             print("Leap year")
         else:
