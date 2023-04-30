8fd530d2fd37ebde54e9d105f797a29b6b808424
kahar <abdkaharmuzakir@gmail.com>
2023-04-30 19:43:46 +0700

    Fix bug in logic

--- day-3-end/pizza_order_practice.py
+++ day-3-end/pizza_order_practice.py
@@ -19,31 +19,51 @@ CLUE
 small_pizza = 15
 medium_pizza = 20
 large_pizza = 25
-total = 0
 
 if size == "S":
     if add_pepperoni == "Y":
         if extra_cheese == "Y":
             total = small_pizza + 1 + 2
             print(total)
-    else:
-        total = small_pizza + 0
-        print(total)
-elif size == "M":
+        elif extra_cheese == "N":
+            total = small_pizza + 2
+            print(total)
+    elif add_pepperoni == "N":
+        if extra_cheese == "Y":
+            total = small_pizza + 1
+            print(total)
+        elif extra_cheese == "N":
+            total = small_pizza + 0
+            print(total)
+if size == "M":
     if add_pepperoni == "Y":
         if extra_cheese == "Y":
             total = medium_pizza + 1 + 3
             print(total)
-    else:
-        total = medium_pizza + 0
-        print(total)
-elif size == "L":
+        elif extra_cheese == "N":
+            total = medium_pizza + 3
+            print(total)
+    elif add_pepperoni == "N":
+        if extra_cheese == "Y":
+            total = medium_pizza + 1
+            print(total)
+        elif extra_cheese == "N":
+            total = medium_pizza + 0
+            print(total)
+if size == "L":
     if add_pepperoni == "Y":
         if extra_cheese == "Y":
             total = large_pizza + 1 + 3
             print(total)
-    else:
-        total = medium_pizza + 0
-        print(total)
+        elif extra_cheese == "N":
+            total = large_pizza + 3
+            print(total)
+    elif add_pepperoni == "N":
+        if extra_cheese == "Y":
+            total = large_pizza + 1
+            print(total)
+        elif extra_cheese == "N":
+            total = medium_pizza + 0
+            print(total)
 
 
