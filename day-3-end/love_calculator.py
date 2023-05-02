ad507553313bca757d5dab5e14630fa21fe6a1d0
kahar <abdkaharmuzakir@gmail.com>
2023-05-02 14:46:27 +0700

    Add love calculator

---
+++ day-3-end/love_calculator.py
@@ -0,0 +1,22 @@
+# Don't change the code below
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# Don't change the code above
+
+#Write your code below this line
+"""
+Instructions
+
+You are going to write a program that tests the compatibility between two people.
+To work out the love score between two people:
+
+Take both people's names and check for the number of times the letters in the word TRUE occurs. 
+
+Then check for the number of times the letters in the word LOVE occurs. 
+
+Then combine these numbers to make a 2 digit number.
+"""
+#For Love Scores less than 10 or greater than 90
+#For Love Scores between 40 and 50
+#Otherwise, the message will just be their score. e.g.:
