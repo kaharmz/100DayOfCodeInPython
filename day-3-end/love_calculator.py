a9afb71eac27a9f8af3601a63e9cafcc7c4616f6
kahar <abdkaharmuzakir@gmail.com>
2023-05-02 21:56:29 +0700

    Add love calculator

--- day-3-end/love_calculator.py
+++ day-3-end/love_calculator.py
@@ -5,18 +5,39 @@ name2 = input("What is their name? \n")
 # Don't change the code above
 
 #Write your code below this line
-"""
-Instructions
 
-You are going to write a program that tests the compatibility between two people.
-To work out the love score between two people:
+merge_name = (name1 + name2).lower()
 
-Take both people's names and check for the number of times the letters in the word TRUE occurs. 
+true_count = 0
+love_count = 0
 
-Then check for the number of times the letters in the word LOVE occurs. 
+#check for the number of times the letters in the word TRUE occurs
+for name_one in set("true"):
+    true_count += merge_name.count(name_one)
 
-Then combine these numbers to make a 2 digit number.
-"""
-#For Love Scores less than 10 or greater than 90
+# check for the number of times the letters in the word LOVE occurs.
+for name_two in set("love"):
+    love_count += merge_name.count(name_two)
+
+# combine these numbers to make a 2 digit number.
+love_score = int(str(true_count) + str(love_count))
+
+if love_score < 10 or love_score > 90:
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif 40 >= love_score <= 50:
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
+
+
+
+
+
+
+
+
+
+
+# For Love Scores less than 10 or greater than 90
 #For Love Scores between 40 and 50
 #Otherwise, the message will just be their score. e.g.:
