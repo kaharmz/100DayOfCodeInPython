c41d9a742a0fe95e76d429475f3bf0e8f6791ceb
kahar <abdkaharmuzakir@gmail.com>
2023-05-05 16:09:17 +0700

    Add file banker_roulette

--- day-4-end/banker_roulette.py
+++ day-4-end/banker_roulette.py
@@ -6,6 +6,6 @@ names = names_string.split(", ")
 # Don't change the code above
 
 #Write your code below this line
-result = random.randrange(len(names))
-random_people = names[result]
-print(f"{random_people} is going to buy the meal today!")
+choice = random.randrange(len(names))
+people_pay = names[choice]
+print(f"{people_pay} is going to buy the meal today!")
