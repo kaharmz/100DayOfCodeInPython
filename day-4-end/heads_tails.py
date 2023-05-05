d2d765fb033308c4691711bc78218b0a186bac14
kahar <abdkaharmuzakir@gmail.com>
2023-05-05 10:52:00 +0700

    Add new solution

--- day-4-end/heads_tails.py
+++ day-4-end/heads_tails.py
@@ -1,5 +1,13 @@
 import random
 
+#Solution One
 random_string = ["Heads", "Tails"]
 results = random.choice(random_string)
 print(results)
+
+#Solution Two
+random_number = random.randint(2, 3)
+if random_number == 2:
+    print('Heads')
+else:
+    print('Tails')
