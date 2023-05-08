e4cdd2d222e8ac7e70f1aa85054345d32d18a327
kahar <abdkaharmuzakir@gmail.com>
2023-05-08 12:13:25 +0700

    Add logic and comment

--- day-4-end/treasure_map.py
+++ day-4-end/treasure_map.py
@@ -9,8 +9,13 @@ position = input("Where do you want to put the treasure? ")
 
 #Write your code below this row
 
+#Cek position input index 0
 column = int(position[0])
+
+#Cek position input index 1
 row = int(position[1])
+
+#mapping column and row, and change with 'X'
 map[row - 1][column - 1] = 'X'
 
 #Write your code above this row
