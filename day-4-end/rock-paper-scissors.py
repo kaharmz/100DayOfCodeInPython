9d95c3a0bb74a9b1e64425272993795ab2a683f3
kahar <abdkaharmuzakir@gmail.com>
2023-05-09 10:32:14 +0700

    Cleancode

--- day-4-end/rock-paper-scissors.py
+++ day-4-end/rock-paper-scissors.py
@@ -1,6 +1,5 @@
 import random
 
-human_chose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 # Rock
 rock = """
     _______
@@ -31,49 +30,39 @@ scissors = """
 ---.__(___)
 """
 
+random_list = [rock, paper, scissors]
+#Human chose
+human_chose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(random_list[human_chose])
 
 #Computer chose
-computer_random = [rock, paper, scissors]
-computer_chose = random.choice(computer_random)
+computer_chose = random.randint(0, 2)
+print("Computer chose:")
+print(random_list[computer_chose])
 
-#Human chose
 if human_chose == 0:
-    print(rock)
-    print("Computer chose:")
-    if computer_chose == computer_random[0]:
-        print(rock)
+    if computer_chose == 0:
         print("Draw!")
-    elif computer_chose == computer_random[1]:
-        print(paper)
+    elif computer_chose == 1:
         print("You lose!")
     else:
-        print(scissors)
         print("You win!")
-
 elif human_chose == 1:
-    print(paper)
-    print("Computer chose:")
-    if computer_chose == computer_random[0]:
-        print(rock)
+    if computer_chose == 0:
         print("You win!")
-    elif computer_chose == computer_random[1]:
-        print(paper)
+    elif computer_chose == 1:
         print("Draw!")
     else:
-        print(scissors)
         print("You lose!")
+elif human_chose == 2:
+    if computer_chose == 0:
+        print("You lose!")
+    elif computer_chose == 1:
+        print("You win!")
+    else:
+        print("Draw!")
 else:
-    print(scissors)
-    print("Computer chose:")
-    if computer_chose == computer_random[0]:
-        print(rock)
-        print("You lose!")
-    elif computer_chose == computer_random[1]:
-        print(paper)
-        print("You win!")
-    else:
-        print(scissors)
-        print("Draw!")
+    print("You typed an invalid number, you lose!")
 
 
 
