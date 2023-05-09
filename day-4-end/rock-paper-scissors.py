1e89d93a8dd9043134851cf890856f34870513a5
kahar <abdkaharmuzakir@gmail.com>
2023-05-09 10:32:14 +0700

    Add "It's a draw"

--- day-4-end/rock-paper-scissors.py
+++ day-4-end/rock-paper-scissors.py
@@ -1,6 +1,6 @@
 import random
 
-human_chose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+human_chose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 # Rock
 rock = """
     _______
@@ -42,7 +42,7 @@ if human_chose == 0:
     print("Computer chose:")
     if computer_chose == computer_random[0]:
         print(rock)
-        print("Draw!")
+        print("It's a draw")
     elif computer_chose == computer_random[1]:
         print(paper)
         print("You lose!")
@@ -58,7 +58,7 @@ elif human_chose == 1:
         print("You win!")
     elif computer_chose == computer_random[1]:
         print(paper)
-        print("Draw!")
+        print("It's a draw")
     else:
         print(scissors)
         print("You lose!")
@@ -73,7 +73,7 @@ else:
         print("You win!")
     else:
         print(scissors)
-        print("Draw!")
+        print("It's a draw")
 
 
 
