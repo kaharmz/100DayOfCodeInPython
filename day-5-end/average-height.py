f5830ff03d1dc539b7c9c0a353d6d900865249ee
kahar <abdkaharmuzakir@gmail.com>
2023-05-10 18:33:11 +0700

    Change variable name

--- day-5-end/average-height.py
+++ day-5-end/average-height.py
@@ -6,13 +6,12 @@ for n in range(0, len(student_heights)):
 # Don't change the code above
 
 #Instructions
-total = 0
-result = 0
+total_index = 0
+total_sum = 0
 #You are going to write a program that calculates the average student height from a List of heights.
 for student in student_heights:
-  total += student
-  result += 1
-average = total / result
+  total_sum += student
+  total_index += 1
+average = total_sum / total_index
 #The average height can be calculated by adding all the heights together and dividing by the total number of heights.
 print(round(average))
-#Write your code below this row
