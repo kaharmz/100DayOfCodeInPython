0ecee19650c3244e8d78678baa544ccc7b4ec0ae
kahar <abdkaharmuzakir@gmail.com>
2023-05-15 10:49:20 +0700

    Add even number file

---
+++ day-5-end/even-number.py
@@ -0,0 +1,10 @@
+#Write your code below this row
+total = 0
+#calculates the sum of all the even numbers from 1 to 100
+for number in range(1, 101):
+    #Cek even number
+    if number % 2 == 0:
+        #add up every even number
+        total += number
+#print total number
+print(total)
