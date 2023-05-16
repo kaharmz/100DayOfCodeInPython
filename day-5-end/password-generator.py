3f1bf1b9743c9d48cfb6b1c8604710922dee4441
kahar <abdkaharmuzakir@gmail.com>
2023-05-16 15:21:15 +0700

    Use choice random function and sample function

--- day-5-end/password-generator.py
+++ day-5-end/password-generator.py
@@ -2,9 +2,26 @@ import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '?', '>', '<', '|', '-', '_', '@']
 
 print("Welcome to the PyPassword Generator!")
 inp_letters = int(input("How many letters would you like in your password?\n"))
 inp_symbols = int(input("How many symbols would you like?\n"))
 inp_numbers = int(input("How many numbers would you like?\n"))
+
+result = []
+for letter in range(inp_letters):
+    rnd_letter = random.choice(letters)
+    result.append(rnd_letter)
+
+for symbol in range(inp_symbols):
+    rnd_symbol = random.choice(symbols)
+    result.append(rnd_symbol)
+
+for number in range(inp_numbers):
+    rnd_number = random.choice(numbers)
+    result.append(rnd_number)
+
+generates = random.sample(result, len(result))
+print(f"Here is your password: {''.join(generates)}")
+
