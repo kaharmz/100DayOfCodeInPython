64b1abbd52f8be2aa3731dad95b1d079377136e5
kaharmz <abdkaharmuzakir@gmail.com>
2024-03-07 16:36:15 +0700

    Clean code

--- day-3-end/odd_or_even.py
+++ day-3-end/odd_or_even.py
@@ -1,6 +1,4 @@
-# Don't change the code below
 number = int(input("Which number do you want to check? "))
-# Don't change the code above
 
 # Write your code below this line
 if number % 2 == 0:
--- day-3-end/pizza_order_practice.py
+++ day-3-end/pizza_order_practice.py
@@ -1,9 +1,7 @@
-# Don't change the code below
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
-# Don't change the code above
 
 """
 CLUE
